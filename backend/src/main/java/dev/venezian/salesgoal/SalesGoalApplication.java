2a318952018274f35c2e42fb08f1f213cf45d6d0
Gabriel Venezian <gabriel.venezian@hotmail.com>
2022-09-06 16:54:13 -0300

    Create project

---
+++ backend/src/main/java/dev/venezian/salesgoal/SalesGoalApplication.java
@@ -0,0 +1,13 @@
+package dev.venezian.salesgoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesGoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesGoalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/venezian/salesgoal/SalesGoalApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.venezian.salesgoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesGoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
